bc8f55189fd435005eb64747e051b5aa46e0acaf
homy <homycoder@gmail.com>
2023-05-25 16:22:59 +0300

    Added custom documentation with code examples

---
+++ src/middlewares/wrong_path.py
@@ -0,0 +1,18 @@
+from src.app import app
+
+from starlette.requests import Request
+from starlette.responses import JSONResponse
+
+
+# Несуществующий путь - миддлварь
+@app.middleware("http")
+async def check_path_exists(request: Request, call_next):
+    path = request.url.path
+
+    if not path.startswith("/"):
+        path = "/" + path
+
+    if path not in app.routes:
+        raise JSONResponse(status_code=404, content={"status": "BAD_REQUEST"})
+
+    return await call_next(request)
---
+++ src/routers/custom_doc.py
@@ -0,0 +1,25 @@
+from starlette.requests import Request
+from starlette.templating import Jinja2Templates
+
+from fastapi.staticfiles import StaticFiles
+
+from pathlib import Path
+
+
+from src.app import app
+
+app.mount(
+    "/src//templates/static",
+    StaticFiles(directory="src/templates/static"),
+    name="static",
+)
+
+templates = Jinja2Templates(directory="src/templates")
+
+@app.get("/docs/example", include_in_schema=False)
+async def custom_doc_handler(request: Request):
+    """
+    Кастомная секция документации - примеры кода
+    """
+    return templates.TemplateResponse("code.html", context={"request": request})
+
