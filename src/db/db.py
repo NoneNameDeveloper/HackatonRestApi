c14fddaec1bcab3993db16de088ca4df14c9a450
homy <homycoder@gmail.com>
2023-05-26 14:12:03 +0300

    Edited connection to database pool type

--- src/db/db.py
+++ src/db/db.py
@@ -1,6 +1,6 @@
-from peewee import *
+from playhouse.pool import PooledPostgresqlDatabase
 from src.data.config import *
 
 # строка подключения к базе данных
-db = PostgresqlDatabase(POSTGRES_DATABASE, user=POSTGRES_USER, password=POSTGRES_PASSWORD, host=POSTGRES_HOST, port=POSTGRES_PORT)
-print("Initialized db")
+db = PooledPostgresqlDatabase(POSTGRES_DATABASE, user=POSTGRES_USER, password=POSTGRES_PASSWORD, host=POSTGRES_HOST, port=POSTGRES_PORT)
+# print("Initialized db")
