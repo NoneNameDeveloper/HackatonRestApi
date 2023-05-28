b960e34fc9c337c6d82b40464f7cd776a198bb4f
homy <homycoder@gmail.com>
2023-05-28 22:59:29 +0300

    piz da

--- example-bot/start_bot.sh
+++ example-bot/start_bot.sh
@@ -1,4 +1,4 @@
 export TELEGRAM_TOKEN="6109375437:AAHODAg-plWR30GEooQRJpHWMRxYUtQ4cSY"
-export TADA_API_BASE_URL="http://127.0.0.1:8081"
+export TADA_API_BASE_URL="http://0.0.0.1:8081"
 export TADA_API_COMPANY_TOKEN="2G_FvgBq_88HMdDKxNu8qQ"
-python3 bot.py
+python3 bot_aio.py
--- src/routers/__init__.py
+++ src/routers/__init__.py
@@ -1,3 +1,4 @@
+from src.routers.rule import *
 from src.routers.conversation import *
 from src.routers.company import *
 from src.routers.root import *
