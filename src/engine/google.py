3c0e1c79b9e7ecf34c6a7edbb39264c47746561d
homy <homycoder@gmail.com>
2023-05-28 21:04:52 +0300

    refactored

--- src/engine/google.py
+++ src/engine/google.py
@@ -5,6 +5,7 @@ api_key = 'AIzaSyCtGE_QI_PU09bF7uAxKb4UgPfaVW_DmD8'
 
 service = build("customsearch", "v1", developerKey=api_key)
 
+
 def search_links(query: str) -> list[str]:
     res = service.cse().list(q=query, cx='86165028723124a88').execute()
     return [item['link'] for item in res['items']]
