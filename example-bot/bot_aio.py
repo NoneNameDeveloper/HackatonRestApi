27737b2b295ba141c65393ca51846a9f50d1d885
homy <homycoder@gmail.com>
2023-06-07 17:45:44 +0300

    shit

--- example-bot/bot_aio.py
+++ example-bot/bot_aio.py
@@ -1,4 +1,5 @@
 import os
+import traceback
 
 import requests
 import aiogram
@@ -7,6 +8,7 @@ from aiogram import types
 import logging
 from urllib.parse import quote
 
+import base64
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
@@ -24,16 +26,33 @@ dp = aiogram.Dispatcher(bot)
 # reset_kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
 # reset_kb.add("/reset")
 
+def encode_to_base64(string) -> str:
+    """
+    кодируем строку в base64
+    """
+    encoded_string = base64.b64encode(string.encode('utf-8'))
+    return encoded_string.decode('utf-8')
+
+
+def decode_from_base64(encoded_string) -> str:
+    """
+    декодируем строку из base64
+    """
+    decoded_string = base64.b64decode(encoded_string).decode('utf-8')
+    return decoded_string
+
 
 def create_user_kb(buttons: list[str]):
     keyboard = types.InlineKeyboardMarkup()
 
-
     for u in buttons:
-        keyboard.add(types.InlineKeyboardButton(u, callback_data=u))
+        try:
+            keyboard.add(types.InlineKeyboardButton(u, callback_data=f"tree_{encode_to_base64(u[:32])}"))
+        except Exception as e:
+            print(traceback.format_exc())
+            pass
 
     return keyboard
-
 
 # @dp.message_handler(commands=["start", "restart", "reset"])
 # async def reset_state_handler(message: types.Message):
@@ -80,12 +99,14 @@ async def all_text_hander(message: types.Message):
     user_id = message.chat.id
 
     try:
-        response_f = requests.get(base_url + "/get_filter?user_id=" + str(user_id) + "&token=" + company_token).json()
+        response_f = requests.get(base_url + "/get_filter?user_id=" + str(user_id) + "&token=" + company_token, timeout=3).json()
 
         if message.text.lower() in response_f['content']:
             return await message.answer("Не допустимый вашей компанией запрос!")
     except:
         pass
+
+    print(base_url + "/text_prompt?user_id=" + str(user_id) + "&token=" + company_token + "&text=" + quote(text))
 
     response = requests.get(base_url + "/text_prompt?user_id=" + str(user_id) + "&token=" + company_token + "&text=" + quote(text)).json()
 
@@ -96,14 +117,34 @@ async def all_text_hander(message: types.Message):
 
     buttons = response['variants']
     n = 3500
-    print(buttons)
+    print("тут")
     [await message.answer(text=s, reply_markup=create_user_kb(buttons)) for s in [answer[i:i+n] for i in range(0, len(answer), n)]]
 
 
-@dp.callback_query_handler(lambda query: True)
-async def handle_button1_click(query: types.CallbackQuery):
-    await query.answer('You clicked Button 1!')
+@dp.callback_query_handler(text_contains="tree_")
+async def handle_tree_buttons_click_handler(call: types.CallbackQuery):
 
-aiogram.executor.start_polling(dp)
+    data = call.data.split("_")  # example: ['tree', 'Экономика']
+
+    # получаем название ноды из ветки вопрос-ответов
+    node_title = decode_from_base64(data[1])
+
+    user_id = call.message.chat.id
+
+    response = requests.get(
+        base_url + "/text_prompt?user_id=" + str(user_id) + "&token=" + company_token + "&text=" + quote(node_title)).json()
+
+    if response['status'] != "SUCCESS":
+        answer = "Ошибка: " + response['STATUS']
+    else:
+        answer = response['result']
+
+    buttons = response['variants']
+    n = 3500
+
+    [await call.message.edit_text(text=s, reply_markup=create_user_kb(buttons)) for s in
+     [answer[i:i + n] for i in range(0, len(answer), n)]]
+
+aiogram.executor.start_polling(dp, skip_updates=True)
 
 
